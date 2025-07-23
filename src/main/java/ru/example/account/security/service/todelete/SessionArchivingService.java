cd01ea1c318c5b0c492240526b2a2dc8dc55736c
Abriel <abrielz11@gmail.com>
2025-07-23 13:04:06 +0300

    ft refactoring security layer so much todo but 95%  done ft toDelete is deleted

--- src/main/java/ru/example/account/security/service/todelete/SessionArchivingService.java
+++
@@ -1,9 +0,0 @@
-package ru.example.account.security.service.todelete;
-
-
-import ru.example.account.security.entity.AuthSession;
-import ru.example.account.security.entity.RevocationReason;
-
-public interface SessionArchivingService {
-    void archive(AuthSession session, RevocationReason reason);
-}
